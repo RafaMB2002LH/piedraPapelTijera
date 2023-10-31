4a3d8d0b58c31ed1cd0132cc1a85908c2be9e265
Rafael Montoro Berdonces <rafaelmontoroberdonces441@gmail.com>
2023-10-31 16:55:27 +0100

    Nueva version, la amquina elige la opcion mas eleg

--- main.py
+++ main.py
@@ -21,6 +21,25 @@ def eleccion_inteligente(eleccion_jugador):
     else:
         return "piedra"
 
+# Función para que la máquina elija una opción basada en la elección predominante del jugador
+def eleccion_inteligente_v2(elecciones_jugador):
+    opciones = ["piedra", "papel", "tijeras"]
+    
+    # Si es la primera jugada, elige una opción aleatoria
+    if not elecciones_jugador:
+        return random.choice(opciones)
+    
+    # Calcula la elección predominante del jugador
+    eleccion_predominante = max(set(elecciones_jugador), key=elecciones_jugador.count)
+    
+    # La máquina elige la opción que habría ganado a la elección predominante del jugador
+    if eleccion_predominante == "piedra":
+        return "papel"
+    elif eleccion_predominante == "papel":
+        return "tijeras"
+    else:
+        return "piedra"
+    
 # Función para determinar el ganador de una partida
 def determinar_ganador(jugador, maquina):
     if jugador == maquina:
@@ -77,5 +96,26 @@ def juego_piedra_papel_tijeras_inteligente():
         
         eleccion_jugador = jugador  # Actualiza la elección previa del jugador
 
+# Función principal del juego
+def juego_piedra_papel_tijeras_inteligente_v2():
+    elecciones_jugador = []  # Para rastrear las elecciones previas del jugador
+    
+    while True:
+        jugador = input("Elige piedra, papel o tijeras (o escribe 'salir' para terminar el juego): ").lower()
+
+        if jugador == "salir":
+            break
+
+        if jugador not in ["piedra", "papel", "tijeras"]:
+            print("Opción no válida. Elige piedra, papel o tijeras.")
+            continue
+
+        maquina = eleccion_inteligente_v2(elecciones_jugador)
+        resultado = determinar_ganador(jugador, maquina)
+        print(f"Jugador eligió {jugador}, Máquina eligió {maquina}. Resultado: {resultado}")
+        guardar_resultado(resultado)
+        
+        elecciones_jugador.append(jugador)  # Agrega la elección del jugador a la lista
+
 if __name__ == "__main__":
-    juego_piedra_papel_tijeras_inteligente()
+    juego_piedra_papel_tijeras_inteligente_v2()
